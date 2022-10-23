e9edf43a8034fa2601a0c31ea011e91d6eb3ceb4
Cristian Paolini <cristianpaolini3@gmail.com>
2022-10-23 16:12:23 -0300

    first commit

---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Button extends Component {
+  render() {
+    return <div>
+        <button class="calcButton">1</button>
+    </div>;
+  }
+}
+
+export default Button;
